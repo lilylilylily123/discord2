c312f4a531fba02b5d4e2bef21b003b2081f543d
Lily G <aksg656@icloud.com>
2023-12-22 10:35:06 -0600

    nice

---
+++ discord2-server/src/audio.rs
@@ -0,0 +1,30 @@
+use cpal::{Data, Sample, SampleFormat, FromSample};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+
+pub fn main() {
+	let host = cpal::default_host();
+	let device = host.default_output_device().expect("no output device available");
+	let mut supported_configs_range = device.supported_output_configs()
+		.expect("error while querying configs");
+	let supported_config = supported_configs_range.next()
+		.expect("no supported config?!")
+		.with_max_sample_rate();
+	let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
+	let sample_format = supported_config.sample_format();
+	let config = supported_config.into();
+	let stream = match sample_format {
+		SampleFormat::F32 => device.build_output_stream(&config, write_silence::<f32>, err_fn, None),
+		SampleFormat::I16 => device.build_output_stream(&config, write_silence::<i16>, err_fn, None),
+		SampleFormat::U16 => device.build_output_stream(&config, write_silence::<u16>, err_fn, None),
+		sample_format => panic!("Unsupported sample format '{sample_format}'")
+	}.unwrap();
+	stream.play().unwrap();
+	println!("PLAYING")
+}
+
+
+fn write_silence<T: Sample>(data: &mut [T], _: &cpal::OutputCallbackInfo) {
+	for sample in data.iter_mut() {
+		*sample = Sample::EQUILIBRIUM;
+	}
+}
